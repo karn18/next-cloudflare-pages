c8308d014ca4e6675c3cc2423bac0d9788308161
Karn Tirasoontorn <karn@hey.com>
2023-02-21 16:56:58 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { fetchAPI } from "../lib/api"
+
+const About = ({ homepage }) => {
+  console.log(homepage)
+  return (
+    <h1>About</h1>
+  )
+}
+
+export default About
+
+export async function getServerSideProps() {
+  // Run API calls in parallel
+  const homepageRes = await fetchAPI("/homepage", {
+    populate: {
+      hero: "*",
+      seo: { populate: "*" },
+    },
+  })
+
+  // console.log(homepageRes)
+
+  return {
+    props: {
+      homepage: homepageRes.data,
+    },
+  }
+}
